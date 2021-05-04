9ab96275c70263c9e4e40fd8672b00b5e1999e3e
Gersón Gómez <gergomez18@gmail.com>
2021-05-04 08:41:09 -0500

    Página de llave en mano

--- layouts/Index/key_on_hand.php
+++ layouts/Index/key_on_hand.php
@@ -1,3 +1,126 @@
 <?php defined('_EXEC') or die; ?>
 
-<main></main>
+<main>
+    <section class="pos-relative" style="height:50vh;">
+        <figure style="height:100%;">
+            <img src="{$path.images}hd_1.png" alt="Background" class="img-cover">
+        </figure>
+        <div class="pos-absolute d-flex align-items-center p-20 p-md-40" style="top:0px;right:0px;bottom:0px;left:0px;background-color:rgba(0,0,0,0.4);"></div>
+    </section>
+    <section class="p-20 p-md-40" style="background-color:#f2f2f2;">
+        <div class="container">
+            <div class="pos-relative">
+                <div class="pos-absolute" style="width:100%;top:-120px;">
+                    <div class="m-b-20 m-b-md-0">
+                        <div class="p-20 d-flex align-items-center justify-content-center flex-column bx-shadow" style="height:180px;background-color:#fff;">
+                            <h2 class="m-b-5">{$lang.hd_tt_1}</h2>
+                            <p class="text-center">{$lang.hd_tt_2}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="p-20 p-md-40" style="background-color:#f2f2f2;">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-md-3"></div>
+                <div class="col-md-3 pos-relative circle" style="border-right:2px solid #9e9e9e;"></div>
+                <div class="col-md-3 pos-relative stick right">
+                    <div class="p-20 d-flex align-items-end justify-content-center flex-column">
+                        <i class="fas fa-hands-helping m-b-20" style="font-size:40px;"></i>
+                        <h4 class="m-b-20 text-right">{$lang.hd_tt_3}</h4>
+                        <p class="text-right">{$lang.hd_tt_4}</p>
+                    </div>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-3"></div>
+                <div class="col-md-3 pos-relative circle" style="border-right:2px solid #9e9e9e;">
+                    <div class="p-20 d-flex align-items-start justify-content-center flex-column">
+                        <i class="fas fa-hands-helping m-b-20" style="font-size:40px;"></i>
+                        <h4 class="m-b-20 text-left">{$lang.hd_tt_5}</h4>
+                        <p class="text-left">{$lang.hd_tt_6}</p>
+                    </div>
+                </div>
+                <div class="col-md-3 stick left"></div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-3"></div>
+                <div class="col-md-3 pos-relative circle" style="border-right:2px solid #9e9e9e;"></div>
+                <div class="col-md-3 stick right">
+                    <div class="p-20 d-flex align-items-end justify-content-center flex-column">
+                        <i class="fas fa-hands-helping m-b-20" style="font-size:40px;"></i>
+                        <h4 class="m-b-20 text-right">{$lang.hd_tt_7}</h4>
+                        <p class="text-right">{$lang.hd_tt_8}</p>
+                    </div>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-3"></div>
+                <div class="col-md-3 pos-relative circle" style="border-right:2px solid #9e9e9e;">
+                    <div class="p-20 d-flex align-items-start justify-content-center flex-column">
+                        <i class="fas fa-hands-helping m-b-20" style="font-size:40px;"></i>
+                        <h4 class="m-b-20 text-left">{$lang.hd_tt_9}</h4>
+                        <p class="text-left">{$lang.hd_tt_10}</p>
+                    </div>
+                </div>
+                <div class="col-md-3 stick left"></div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-3"></div>
+                <div class="col-md-3 pos-relative circle" style="border-right:2px solid #9e9e9e;"></div>
+                <div class="col-md-3 stick right">
+                    <div class="p-20 d-flex align-items-end justify-content-center flex-column">
+                        <i class="fas fa-hands-helping m-b-20" style="font-size:40px;"></i>
+                        <h4 class="m-b-20 text-right">{$lang.hd_tt_11}</h4>
+                        <p class="text-right">{$lang.hd_tt_12}</p>
+                    </div>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-3"></div>
+                <div class="col-md-3 pos-relative circle" style="border-right:2px solid #9e9e9e;">
+                    <div class="p-20 d-flex align-items-start justify-content-center flex-column">
+                        <i class="fas fa-hands-helping m-b-20" style="font-size:40px;"></i>
+                        <h4 class="m-b-20 text-left">{$lang.hd_tt_13}</h4>
+                        <p class="text-left">{$lang.hd_tt_14}</p>
+                    </div>
+                </div>
+                <div class="col-md-3 stick left"></div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-3"></div>
+                <div class="col-md-3 pos-relative circle" style="border-right:2px solid #9e9e9e;"></div>
+                <div class="col-md-3 stick right">
+                    <div class="p-20 d-flex align-items-end justify-content-center flex-column">
+                        <i class="fas fa-hands-helping m-b-20" style="font-size:40px;"></i>
+                        <h4 class="m-b-20 text-right">{$lang.hd_tt_15}</h4>
+                        <p class="text-right">{$lang.hd_tt_16}</p>
+                    </div>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+    </section>
+    <section class="pos-relative" style="height:50vh;">
+        <figure style="height:100%;">
+            <img src="{$path.images}hd_2.png" alt="Background" class="img-cover">
+        </figure>
+        <div class="pos-absolute d-flex align-items-center p-20 p-md-40" style="top:0px;right:0px;bottom:0px;left:0px;background-color:rgba(0,0,0,0.4);">
+            <div class="container">
+                <h1 class="m-b-20 text-center text-light">{$lang.hd_tt_17}</h1>
+                <p class="m-b-20 text-center text-light">{$lang.hd_tt_18}</p>
+                <div class="d-flex justify-content-center">
+                    <a href="/contactanos" class="btn" style="width:100%;max-width:200px;border:0px;border-radius:40px;background-color:#263689;">{$lang.contact_us}</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
